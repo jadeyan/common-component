c0ff871ec64e9b39f6f16c51f1f1a155eb7ce372
George <chengdonghou@creditease.cn>
2015-11-12 15:39:16 +0800

    Android 6.0 权限动态获取适配

--- app/src/main/java/com/creditease/common/SMSAutoGetActivity.java
+++ app/src/main/java/com/creditease/common/SMSAutoGetActivity.java
@@ -1,14 +1,20 @@
 package com.creditease.common;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * 类描述：自动获取短信验证码
@@ -23,6 +29,8 @@ public class SMSAutoGetActivity extends Activity {
     private static final int YZM_START = 5;//短信验证码开始的地方
     private static final int YZM_LENGTH = 6;//验证码的长度
 
+    private static final int SMS_RECEIVE_CODE = 1024;//获取权限吗
+
 
     private EditText mSMSShow;
 
@@ -33,11 +41,66 @@ public class SMSAutoGetActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sms_layout);
         mSMSShow = (EditText) findViewById(R.id.et_sms);
-        //动态注册广播接收器，动态注册的优先级要比静态注册的搞
+        if(Build.VERSION.SDK_INT >= 23){
+            smsReceiverPermissionAsk();
+        }else{
+            registerSMSReceive();
+        }
+
+    }
+
+    //动态权限设置for Android 6.0
+    private void smsReceiverPermissionAsk(){
+        int hasReceiveSMSPermission = checkSelfPermission(Manifest.permission.RECEIVE_SMS);
+        if(hasReceiveSMSPermission != PackageManager.PERMISSION_GRANTED){
+            if(!shouldShowRequestPermissionRationale(Manifest.permission.RECEIVE_SMS)){//用户之前已经选择了下次不再提醒
+                smsPermissionAskDialog("获取自动读取验证码权限", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        requestPermissions(new String[]{Manifest.permission.RECEIVE_SMS}, SMS_RECEIVE_CODE);
+                    }
+                });
+            }
+            requestPermissions(new String[]{Manifest.permission.RECEIVE_SMS}, SMS_RECEIVE_CODE);
+            return;
+        }
+        registerSMSReceive();
+    }
+
+    //用户权限选择时弹框提示
+    private void smsPermissionAskDialog(String message,DialogInterface.OnClickListener okClick){
+        new AlertDialog.Builder(SMSAutoGetActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK",okClick)
+                .setNegativeButton("cancel",null)
+                .create()
+                .show();
+
+    }
+
+    //动态注册广播接收器，动态注册的优先级要比静态注册的搞
+    private void registerSMSReceive(){
         IntentFilter mfilter = new IntentFilter();
         mfilter.addAction("android.provider.Telephony.SMS_RECEIVED");
         mfilter.setPriority(Integer.MAX_VALUE);//虽然API文档说最多1000，但是大了也没问题
         registerReceiver(new SMSReceive(), mfilter);
+    }
+
+    //权限设置后回调
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode){
+            case SMS_RECEIVE_CODE:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    registerSMSReceive();
+                }else{
+                    Toast.makeText(this,"您已经拒绝了自动获取短信验证码",Toast.LENGTH_LONG).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                break;
+        }
     }
 
     class SMSReceive extends BroadcastReceiver {
